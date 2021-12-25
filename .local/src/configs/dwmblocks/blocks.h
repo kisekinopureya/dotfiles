fa9fe3faea124bcbdbf63cc7765efea29106a4a6
kisekinopureya <gokhanozdemir753@gmail.com>
2021-12-25 03:15:52 +0300

    dwmblocks: update sb-battery with apm scripts

--- .local/src/configs/dwmblocks/blocks.h
+++ .local/src/configs/dwmblocks/blocks.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{" ", "setxkbmap -print -verbose 8 | awk /layout/'{print $2}'",		60,		1},
-	{"", "sb-battery",							60,		0},
+	{"", "sb-battery",							60,		2},
 	{"", "sb-volume",							0,		1},
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
@@ -10,3 +10,4 @@ static const Block blocks[] = {
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
+
